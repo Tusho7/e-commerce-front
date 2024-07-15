d6ed3301ac5340214fcdd6b6a73713a53834ef2e
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-15 12:57:38 +0400

    refactor: Update Favorites page layout and styling

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -59,53 +59,57 @@ const Favorites = () => {
       </div>
       <div className="w-full h-[1px] bg-gray-700"></div>
       {dropdown && <DropDown />}
-      <div className="flex flex-wrap gap-2 py-4 px-4">
+      <div className="py-4 px-4">
         {wishlistsData.length > 0 ? (
-          wishlistsData.map((item) => (
-            <div
-              key={item.productId}
-              className="flex-none w-[135px] max-w-[135px]"
-            >
-              <div className="aspect-w-3 aspect-h-2 h-[100px] bg-white flex justify-center items-center p-1 mb-2 rounded-lg">
-                <img
-                  src={`${import.meta.env.VITE_API_STORAGE}${
-                    item.product.images.set[0]
-                  }`}
-                  alt={removeQuotes(item.product.name)}
-                />
+          <div className="flex flex-wrap justify-between">
+            {wishlistsData.map((item) => (
+              <div
+                key={item.productId}
+                className="flex-none w-[135px] max-w-[135px]"
+              >
+                <div className="aspect-w-3 aspect-h-2 h-[100px] bg-white flex justify-center items-center p-1 mb-2 rounded-lg">
+                  <img
+                    src={`${import.meta.env.VITE_API_STORAGE}${
+                      item.product.images.set[0]
+                    }`}
+                    alt={removeQuotes(item.product.name)}
+                  />
+                </div>
+
+                <div className="text-white flex flex-col gap-[1px]">
+                  <h3 className="text-base font-semibold">
+                    {removeQuotes(item.product.name)}
+                  </h3>
+                  <p className="text-sm mb-2">
+                    {truncateDescription(
+                      removeQuotes(item.product.description)
+                    )}
+                  </p>
+                  <p className="text-base font-bold">
+                    {removeQuotes(item.product.price)}
+                  </p>
+                  <p className="text-sm">Stock: {item.product.stock}</p>
+
+                  <section className="flex justify-between items-center gap-5 mt-3">
+                    <button className="bg-black text-white text-xs px-2 py-[10px] rounded-md">
+                      Add to Cart
+                    </button>
+
+                    <div
+                      className="border-black flex justify-center rounded-md items-center p-[6px] bg-white cursor-pointer"
+                      onClick={() => removeFromWishlist(item.productId)}
+                    >
+                      <img
+                        src={Wishlisted}
+                        className="w-6 h-6"
+                        alt="Wishlisted"
+                      />
+                    </div>
+                  </section>
+                </div>
               </div>
-
-              <div className="text-white flex flex-col gap-[1px]">
-                <h3 className="text-base font-semibold">
-                  {removeQuotes(item.product.name)}
-                </h3>
-                <p className="text-sm mb-2">
-                  {truncateDescription(removeQuotes(item.product.description))}
-                </p>
-                <p className="text-base font-bold">
-                  {removeQuotes(item.product.price)}
-                </p>
-                <p className="text-sm">Stock: {item.product.stock}</p>
-
-                <section className="flex justify-between items-center gap-5 mt-3">
-                  <button className="bg-black text-white text-xs px-2 py-[10px] rounded-md">
-                    Add to Cart
-                  </button>
-
-                  <div
-                    className="border-black flex justify-center rounded-md items-center p-[6px] bg-white cursor-pointer"
-                    onClick={() => removeFromWishlist(item.productId)}
-                  >
-                    <img
-                      src={Wishlisted}
-                      className="w-6 h-6"
-                      alt="Wishlisted"
-                    />
-                  </div>
-                </section>
-              </div>
-            </div>
-          ))
+            ))}
+          </div>
         ) : (
           <div className="text-white flex flex-col items-center py-10">
             <p className="text-lg mb-2">Your wishlist is empty!</p>
