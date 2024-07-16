ef7d00ac45c2c105e718d7c5afdd62a46c522c61
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-16 15:09:03 +0400

    refactor: Update Dropdown component to display user profile picture

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -3,10 +3,18 @@ import { useUser } from "../contexts/UseUser";
 
 const Dropdown = () => {
   const { user } = useUser();
+
   return (
     <div className="absolute right-4 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
-      <div>
-        <p className="text-center px-4 py-2 text-sm text-gray-700">
+      <div className="flex gap-2 items-center px-4 py-2">
+        <img
+          src={`${import.meta.env.VITE_API_STORAGE}${
+            user?.user.profilePicture
+          }`}
+          alt="profile_picture"
+          className="rounded-full w-10 h-10"
+        />
+        <p className="text-center  text-sm text-gray-700">
           Welcome,
           <span className="font-semibold text-gray-00">
             {user?.user?.firstName} {user?.user.lastName}
