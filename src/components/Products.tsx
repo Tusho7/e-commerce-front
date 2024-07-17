1782abe7d864081c9e4794e4baf6e93068dfe9f1
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-17 10:05:22 +0400

    refactor: Update Products component layout and styling for better readability

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -124,80 +124,93 @@ const Products = ({ products, setProducts }: ProductContextType) => {
   };
 
   return (
-    <div className="flex overflow-x-auto gap-4 py-4 pl-4 min-h-screen">
-      {products.map((product) => (
-        <div key={product.id} className="flex-none w-[135px] max-w-[135px]">
-          <div className="aspect-w-3 aspect-h-2 h-[100px] bg-white flex justify-center items-center p-1 mb-2 rounded-lg">
-            <img
-              src={`${import.meta.env.VITE_API_STORAGE}${
-                product.images.set[0]
-              }`}
-              alt={product.name}
-              className=""
-            />
-          </div>
+    <div className="py-4 pl-4 min-h-screen flex flex-col gap-5 text-white">
+      <h1>Product List</h1>
+      <div className="flex overflow-x-auto gap-4 ">
+        {products.map((product) => (
+          <div key={product.id} className="flex-none w-[135px] max-w-[135px]">
+            <div className="aspect-w-3 aspect-h-2 h-[100px] bg-white flex justify-center items-center p-1 mb-2 rounded-lg">
+              <img
+                src={`${import.meta.env.VITE_API_STORAGE}${
+                  product.images.set[0]
+                }`}
+                alt={product.name}
+                className=""
+              />
+            </div>
 
-          <div className="text-white flex flex-col gap-[1px]">
-            <h3 className="text-base font-semibold">
-              {removeQuotes(product.name)}
-            </h3>
-            <p className="text-sm mb-2">
-              {truncateDescription(removeQuotes(product.description))}
-            </p>
-            <p className="text-base font-bold">{removeQuotes(product.price)}</p>
-            <p className="text-sm">Stock: {product.stock}</p>
+            <div className="text-white flex flex-col gap-[1px]">
+              <h3 className="text-base font-semibold">
+                {removeQuotes(product.name)}
+              </h3>
+              <p className="text-sm mb-2">
+                {truncateDescription(removeQuotes(product.description))}
+              </p>
+              <p className="text-base font-bold">
+                {removeQuotes(product.price)}
+              </p>
+              <p className="text-sm">Stock: {product.stock}</p>
 
-            <section className="flex justify-between items-center gap-5 mt-3">
-              {product.cart.length > 0 ? (
-                <div>
-                  <Link
-                    className="bg-black text-white text-xs px-2 py-[10px] rounded-md"
-                    to="/cart"
-                  >
-                    See Cart
-                  </Link>
-                </div>
-              ) : (
-                <button
-                  className={`bg-black text-white text-xs px-2 py-[10px] rounded-md ${
-                    product.inCart ? "opacity-50" : ""
-                  }`}
-                  onClick={() => {
-                    if (!product.inCart) {
-                      openModal(product);
-                    } else if (product.inCart || product.cart.length > 0) {
-                      handleAddToCart(product.id, 1, "", "");
-                    }
-                  }}
-                >
-                  {product.inCart || product.cart.length > 0
-                    ? "Remove"
-                    : "Add to Cart"}
-                </button>
-              )}
-
-              <div
-                className="border-black flex justify-center rounded-md items-center p-[6px] bg-white cursor-pointer"
-                onClick={() => toggleWishlist(product)}
-              >
-                {product.wishlist.length > 0 ? (
-                  <img src={Wishlisted} className="w-6 h-6" alt="wishlisted" />
+              <section className="flex justify-between items-center gap-5 mt-3">
+                {product.cart.length > 0 ? (
+                  <div>
+                    <Link
+                      className="bg-black text-white text-xs px-2 py-[10px] rounded-md"
+                      to="/cart"
+                    >
+                      See Cart
+                    </Link>
+                  </div>
                 ) : (
-                  <img src={WishListIcon} className="w-6 h-6" alt="Wishlist" />
+                  <button
+                    className={`bg-black text-white text-xs px-2 py-[10px] rounded-md ${
+                      product.inCart ? "opacity-50" : ""
+                    }`}
+                    onClick={() => {
+                      if (!product.inCart) {
+                        openModal(product);
+                      } else if (product.inCart || product.cart.length > 0) {
+                        handleAddToCart(product.id, 1, "", "");
+                      }
+                    }}
+                  >
+                    {product.inCart || product.cart.length > 0
+                      ? "Remove"
+                      : "Add to Cart"}
+                  </button>
                 )}
-              </div>
-            </section>
+
+                <div
+                  className="border-black flex justify-center rounded-md items-center p-[6px] bg-white cursor-pointer"
+                  onClick={() => toggleWishlist(product)}
+                >
+                  {product.wishlist.length > 0 ? (
+                    <img
+                      src={Wishlisted}
+                      className="w-6 h-6"
+                      alt="wishlisted"
+                    />
+                  ) : (
+                    <img
+                      src={WishListIcon}
+                      className="w-6 h-6"
+                      alt="Wishlist"
+                    />
+                  )}
+                </div>
+              </section>
+            </div>
           </div>
-        </div>
-      ))}
-      {modalProduct && (
-        <AddToCartModal
-          product={modalProduct}
-          isOpen={isModalOpen}
-          onClose={closeModal}
-          onAddToCart={handleAddToCart}
-        />
-      )}
+        ))}
+        {modalProduct && (
+          <AddToCartModal
+            product={modalProduct}
+            isOpen={isModalOpen}
+            onClose={closeModal}
+            onAddToCart={handleAddToCart}
+          />
+        )}
+      </div>
     </div>
   );
 };
