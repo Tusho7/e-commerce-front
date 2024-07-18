998fb0298487788906edd375b4410a7d10ba33c1
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-18 15:00:57 +0400

    refactor: Improve image rendering and styling in Favorites page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -142,23 +142,24 @@ const Favorites = () => {
       {dropdown && <DropDown />}
       <div className="py-4 px-4 min-h-screen">
         {wishlistsData.length > 0 ? (
-          <div className="flex flex-wrap justify-between">
+          <div className="flex flex-wrap justify-between gap-10">
             {wishlistsData.map((item) => (
               <div
                 key={item.productId}
                 className="flex-none w-[135px] max-w-[135px]"
               >
-                <div className="aspect-w-3 aspect-h-2 h-[100px] bg-white flex justify-center items-center p-1 mb-2 rounded-lg">
+                <div className="aspect-w-3 aspect-h-2 h-[100px] bg-white flex justify-center items-center mb-2 rounded-lg">
                   <img
                     src={`${import.meta.env.VITE_API_STORAGE}${
                       item.product.images.set[0]
                     }`}
                     alt={removeQuotes(item.product.name)}
+                    className="w-full h-full rounded-lg"
                   />
                 </div>
 
                 <div className="text-white flex flex-col gap-[1px]">
-                  <h3 className="text-base font-semibold">
+                  <h3 className="text-xs font-semibold">
                     {removeQuotes(item.product.name)}
                   </h3>
                   <p className="text-sm mb-2">
@@ -166,7 +167,7 @@ const Favorites = () => {
                       removeQuotes(item.product.description)
                     )}
                   </p>
-                  <p className="text-base font-bold">
+                  <p className="text-xs font-bold">
                     {removeQuotes(item.product.price)}
                   </p>
                   <p className="text-sm">Stock: {item.product.stock}</p>
