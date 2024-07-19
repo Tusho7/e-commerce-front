ab04cb69bda8e6a2200b67735766dd4bd95e8823
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-19 11:23:25 +0400

    refactor: Add dropdown functionality to ContactPage

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ContactUs from "../components/ContactUs";
 import Footer from "../components/Footer";
-
+import { Link } from "react-router-dom";
+import UserIcon from "../assets/user.png";
+import DropDown from "../components/DropDown";
 const ContactPage: React.FC = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleDropdown = () => {
+    setDropdown((prev) => !prev);
+  };
   return (
     <div>
-      <header className="bg-gray-800 text-white p-4">
-        <div className="container mx-auto"></div>
-      </header>
+      <div className="text-white p-4 flex justify-between items-center">
+        <div className="flex justify-between items-center gap-10 text-sm">
+          <Link to="/home">Home</Link>
+        </div>
+
+        <div onClick={handleDropdown}>
+          <img src={UserIcon} alt="user_icon" className="w-6 h-6" />
+        </div>
+      </div>
+      <div className="w-full h-[1px] bg-gray-700"></div>
+      {dropdown && <DropDown />}
       <main className="my-8">
         <ContactUs />
       </main>
