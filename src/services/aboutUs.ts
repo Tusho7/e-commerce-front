b300c6c2b8185d7f7321ddb01ae13d31b6e08880
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-24 15:10:08 +0400

    feat: add edit functionality in admin panel about us

--- src/services/aboutUs.ts
+++ src/services/aboutUs.ts
@@ -1,5 +1,13 @@
 import axiosInstance from "../plugins/axios/index";
+import { AboutUsProps } from "../types/aboutUs";
 
 export const getAboutUsData = async () => {
   return await axiosInstance.get("/get_aboutus");
+};
+
+export const updateAboutUsData = async (
+  id: number,
+  updatedData: AboutUsProps
+) => {
+  return axiosInstance.put(`/update_aboutus/${id}`, updatedData);
 };
