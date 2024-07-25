aed55541ee43dc3d2e5f72919b323ef03cc770ad
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-25 10:40:36 +0400

    feat: create functionality to add new faq data

--- src/services/faqs.ts
+++ src/services/faqs.ts
@@ -1,5 +1,10 @@
 import axiosInstance from "../plugins/axios/index";
+import { Faq } from "../types/faq";
 
 export const getFaqs = async () => {
   return await axiosInstance.get("/get_faqs");
+};
+
+export const createFaq = async (faq: Faq) => {
+  return await axiosInstance.post("/add_faq", faq);
 };
