59fa7637818e356493f2fffa9e55017e52bdc69a
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-26 10:15:55 +0400

    feat: create modal to add new terms & conditions Fixes #58

--- src/services/terms.ts
+++ src/services/terms.ts
@@ -1,5 +1,10 @@
 import axiosInstance from "../plugins/axios/index";
+import { TermsProps } from "../types/terms";
 
 export const getTermsData = async () => {
   return await axiosInstance.get("/terms_and_conditions");
+};
+
+export const createTerms = async (data: TermsProps) => {
+  return await axiosInstance.post("/create_terms_and_conditions", data);
 };
