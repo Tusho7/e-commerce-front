69bc119ef97c5b43f0ee7d9568debfc41f8d9002
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-29 13:57:59 +0400

    feat: add delete functionality in messages Fixes #74

--- src/services/messages.ts
+++ src/services/messages.ts
@@ -3,3 +3,7 @@ import axiosInstance from "../plugins/axios/index";
 export const getMessages = async () => {
   return await axiosInstance.get("/messages");
 };
+
+export const deleteMessage = async (id: number) => {
+  return await axiosInstance.delete(`/delete_message/${id}`);
+};
