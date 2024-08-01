73d50ee953f53097a257169ff53588d52951bb9c
Tusho7 <sandrotushurashvili7@gmail.com>
2024-08-01 10:45:05 +0400

    refactor: optimize rendering of review component

--- src/pages/Reviews.tsx
+++ src/pages/Reviews.tsx
@@ -4,25 +4,26 @@ const Reviews = ({ reviews }: ReviewProps) => {
   return (
     <div className="relative overflow-hidden w-full mb-20 mt-10">
       <div className="flex animate-slide">
-        {reviews.concat(reviews).map((review, index) => (
-          <div key={index} className="flex-none w-full md:w-1/4 px-4">
-            <div className="bg-white p-6 rounded-lg shadow-lg mx-auto h-[350px] flex flex-col justify-between items-center">
-              <section className="flex flex-col gap-3 items-center justify-center">
-                <img
-                  src={`${import.meta.env.VITE_API_STORAGE}${
-                    review.user.profilePicture
-                  }`}
-                  alt={`${review.user.firstName} ${review.user.lastName}`}
-                  className="rounded-full w-16 h-16 object-cover mb-4 mx-auto border-2 border-blue-500"
-                />
-                <p className="italic text-gray-600 mb-3">{review.quote}</p>
-              </section>
-              <p className="font-bold text-gray-800 text-lg">
-                - {review.user.firstName} {review.user.lastName}
-              </p>
+        {reviews &&
+          reviews.concat(reviews).map((review, index) => (
+            <div key={index} className="flex-none w-full md:w-1/4 px-4">
+              <div className="bg-white p-6 rounded-lg shadow-lg mx-auto h-[350px] flex flex-col justify-between items-center">
+                <section className="flex flex-col gap-3 items-center justify-center">
+                  <img
+                    src={`${import.meta.env.VITE_API_STORAGE}${
+                      review.user.profilePicture
+                    }`}
+                    alt={`${review.user.firstName} ${review.user.lastName}`}
+                    className="rounded-full w-16 h-16 object-cover mb-4 mx-auto border-2 border-blue-500"
+                  />
+                  <p className="italic text-gray-600 mb-3">{review.quote}</p>
+                </section>
+                <p className="font-bold text-gray-800 text-lg">
+                  - {review.user.firstName} {review.user.lastName}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
